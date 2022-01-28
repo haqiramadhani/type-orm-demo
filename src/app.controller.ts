9cde6e71b7662ae682b05f1dea27ecfa21879fef
Haqi Ramadhani <hakiramadhani@gmail.com>
2022-01-28 21:29:08 +0700

    TypeORM Tutorial: Migrations, queries, and more with NestJS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from './user.entity';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,33 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('users')
+  createUser(@Body('name') name: string): Promise<User> {
+    return this.appService.createUser(name);
+  }
+
+  @Get('users')
+  getUsers(): Promise<User[]> {
+    return this.appService.getAll();
+  }
+
+  @Get('users/:id')
+  getUserById(@Param('id') id: string): Promise<User> {
+    return this.appService.getOneById(Number(id));
+  }
+
+  @Patch('users/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body('name') name: string,
+  ): Promise<User> {
+    return this.appService.updateUser(Number(id), name);
+  }
+
+  @Delete('users/:id')
+  deleteUser(@Param('id') id: string): Promise<User> {
+    return this.appService.deleteUser(Number(id));
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.userRepository.find({
+      relations: ['pets'],
+    });
+  }
+
+  async getOneById(id: number): Promise<User> {
+    try {
+      return this.userRepository.findOneOrFail(id);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  createUser(name: string): Promise<User> {
+    const newUser = this.userRepository.create({ name });
+    return this.userRepository.save(newUser);
+  }
+
+  async updateUser(id: number, name: string): Promise<User> {
+    const user = await this.getOneById(id);
+    user.name = name;
+    return this.userRepository.save(user);
+  }
+
+  async deleteUser(id: number): Promise<User> {
+    const user = await this.getOneById(id);
+    return this.userRepository.remove(user);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
